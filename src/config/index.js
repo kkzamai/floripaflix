22a9463ce280666e96f789098d1c8ba2dd7c0323
kkzamai <kkzamai@gmail.com>
2020-08-08 14:31:19 -0300

    Update link

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:5000'
-  : 'http://<meulink>.herokuapp.com';
+  : 'http://floripaflix.herokuapp.com';
 
 export default {
   URL_BACKEND_TOP,
