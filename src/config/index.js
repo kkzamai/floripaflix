4ae46d6cf136e181b588df0ec505d094701b3243
kkzamai <kkzamai@gmail.com>
2020-08-08 15:07:31 -0300

    ajustes

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'http://floripaflix.herokuapp.com';
+  : 'https://floripaflix.herokuapp.com';
 
 export default {
   URL_BACKEND_TOP,
